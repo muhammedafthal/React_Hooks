5be3db2aa9e0f01e86315dcb5e256cccc98a0b5a
Muhammed afthal k <muhammedafthal786gmail.com@Muhammeds-Air.lan>
2025-02-21 02:12:56 -0800

    Just_Finish_useState()_Hook

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,37 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-
-  const [color, setColor] =  useState("Red");
+  
+  const [car, setCar] = useState({
+    brand: "Ferrari",
+    color: "Red",
+    model: "roma",
+    year: "2023"
+  })
 
   const changeColor = () => {
-      setColor("Blue")    
+    setCar((car) => {
+      return {
+        ...car, color: "Blue"
+      }
+    })
+  }
+
+  const [count, setCount] = useState(0);
+  
+  const increaseCount = () => {
+    setCount((prev) => {
+      return prev + 1
+    })
   }
 
   return ( 
     <>
-    <h1>My Favourite Color Is {color}</h1>
-    <button onClick={changeColor}>red</button>
+    <h1>My {car.brand} </h1>
+    <h2>it is a {car.color} {car.model} from {car.year}</h2>
+    <button onClick={changeColor}>Blue</button>
+    <h1>{count}</h1>
+    <button onClick={increaseCount}>Increase</button>
     </>
    )
 }
